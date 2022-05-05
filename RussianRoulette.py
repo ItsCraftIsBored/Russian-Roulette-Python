4b14c2c3e324d96806e40ee017bc12c6276d446d
ItsCraftIsBored <athaya@itscraftsoftware.my.id>
2022-05-05 10:21:12 +0700

    Add files via upload

---
+++ RussianRoulette.py
@@ -0,0 +1,17 @@
+import random
+print("Russian Roulette Python")
+print("Version 1.0")
+print("By itscraftdev")
+print("type pull to start the game")
+print("1 = You Win 2 = You Lose")
+bullet=random.randint(1, 2)
+sys = input()
+if sys == "pull":
+    print(bullet)
+
+
+
+
+                                           
+
+
